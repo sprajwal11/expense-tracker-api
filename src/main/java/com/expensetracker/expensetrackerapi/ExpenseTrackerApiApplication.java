c9c37b386a789fe36f45bb0b8af79cd09a1e0b18
Prajwal <sharmaprajwal11@gmail.com>
2022-09-16 21:23:45 +0530

    JWT Authorization

--- src/main/java/com/expensetracker/expensetrackerapi/ExpenseTrackerApiApplication.java
+++ src/main/java/com/expensetracker/expensetrackerapi/ExpenseTrackerApiApplication.java
@@ -1,7 +1,10 @@
 package com.expensetracker.expensetrackerapi;
 
+import com.expensetracker.expensetrackerapi.filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExpenseTrackerApiApplication {
@@ -10,4 +13,13 @@ public class ExpenseTrackerApiApplication {
 		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterRegistrationBean(){
+		FilterRegistrationBean<AuthFilter> registrationBean=new FilterRegistrationBean<>();
+		AuthFilter authFilter=new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/api/categories/*");
+		return registrationBean;
+
+	}
 }
--- src/main/java/com/expensetracker/expensetrackerapi/resources/UserResource.java
+++ src/main/java/com/expensetracker/expensetrackerapi/resources/UserResource.java
@@ -1,12 +1,17 @@
 package com.expensetracker.expensetrackerapi.resources;
 
+import com.expensetracker.expensetrackerapi.Constants;
 import com.expensetracker.expensetrackerapi.domain.User;
 import com.expensetracker.expensetrackerapi.servicies.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.yaml.snakeyaml.scanner.Constant;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,9 +27,8 @@ public class UserResource {
         String email=(String) userMap.get("email");
         String password=(String) userMap.get("password");
         User user=userService.validateUser(email,password);
-        Map<String, String> map=new HashMap<>();
-        map.put("message", "loggedIn successfully");
-        return new ResponseEntity<>(map,HttpStatus.OK);
+
+        return new ResponseEntity<>(generateJWTToken(user),HttpStatus.OK);
     }
 
     @PostMapping("/register")
@@ -36,8 +40,24 @@ public class UserResource {
         String password=(String) userMap.get("password");
         User user=user=userService.registerUser(firstName,lastName,email,password);
         Map<String,String> map=new HashMap<>();
-        map.put("message", "registered successfully");
-        return new ResponseEntity<>(map, HttpStatus.OK);
+        return new ResponseEntity<>(generateJWTToken(user), HttpStatus.OK);
+    }
+
+    private Map<String,String> generateJWTToken(User user){
+        long timeStamp= System.currentTimeMillis();
+        String token= Jwts.builder().signWith(SignatureAlgorithm.HS256, Constants.API_SECRET_KEY)
+                .setIssuedAt(new Date(timeStamp))
+                .setExpiration(new Date((timeStamp+Constants.TOKEN_VALIDITY)))
+                .claim("userId", user.getUserId())
+                .claim("email",user.getEmail())
+                .claim("firstName",user.getFirstName())
+                .claim("lastName",user.getLastName())
+                .compact();
+        Map<String, String> map=new HashMap<>();
+        map.put("token",token);
+        return map;
+
+
     }
 
 }
