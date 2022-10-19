6adae8bcf9b141ff7738f68b1c10abca78f2ebd3
Prajwal <sharmaprajwal11@gmail.com>
2022-10-19 15:38:40 +0530

    Minor changes for Frontend access

--- src/main/java/com/expensetracker/expensetrackerapi/ExpenseTrackerApiApplication.java
+++ src/main/java/com/expensetracker/expensetrackerapi/ExpenseTrackerApiApplication.java
@@ -2,12 +2,12 @@ package com.expensetracker.expensetrackerapi;
 
 import com.expensetracker.expensetrackerapi.filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 //@Configuration
@@ -19,6 +19,17 @@ public class ExpenseTrackerApiApplication {
 		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		FilterRegistrationBean<CorsFilter> registrationBean=new FilterRegistrationBean<>();
+		UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config=new CorsConfiguration();
+		config.addAllowedHeader("*");
+		registrationBean.setFilter(new CorsFilter(source));
+		registrationBean.setOrder(0);
+		return  registrationBean;
+
+	}
 	@Bean
 	public FilterRegistrationBean<AuthFilter> filterRegistrationBean(){
 		FilterRegistrationBean<AuthFilter> registrationBean=new FilterRegistrationBean<>();
